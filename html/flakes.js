8d3820a53d8fc1c940731605b01cd043159fbf39
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-03-19 13:07:51 +0100

    init: flakes page
    Inspired by NIPA [1] [2].
    
    The goal is to show unstable tests.
    
    Link: https://github.com/linux-netdev/nipa [1]
    Link: https://netdev.bots.linux.dev/flakes.html [2]
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

---
+++ html/flakes.js
@@ -0,0 +1,113 @@
+/*  Inspired by NIPA's flakes: https://github.com/linux-netdev/nipa */
+function colorify(cell, result)
+{
+  var ret;
+  var cmt;
+
+  if (result == null || !("result" in result)) {
+    ret = "";
+    cmt = "";
+  } else {
+    ret = result.result;
+    cmt = result.comment;
+  }
+
+  if (ret == "") { // We only track failed tests: assumed they were OK
+    style = "background-color:green";
+  } else if (ret == "skip") {
+    style = "background-color:blue";
+  } else {
+    style = "background-color:red";
+  }
+
+  if (cmt != "")
+    cell.innerHTML = "<span title=\"" + cmt + "\" data-toggle=\"tooltip\"/>";
+  cell.setAttribute("style", style);
+}
+
+function load_result_table(data_raw)
+{
+  let warn_box = document.getElementById("fl-warn-box");
+  warn_box.innerHTML = "";
+
+  // Get all tags names
+  var tags = new Array();
+  $.each(data_raw, function(i, item) {
+    tags.push([item.tag, item.run_id]);
+  });
+
+  var test_row = {};
+  var tags_errors = new Set(); // TODO: handle errors
+
+  /* Format:
+    [{"results":
+        {"<file>":
+          {<id>:
+            {"result": "<pass/fail>", "name": "<name>", "comment": "<cmt>"}
+          }
+        }
+      "run_id": "<run_id>",
+      "tag": "<tag>"
+    }]
+   */
+  $.each(data_raw, function(i, item) {
+    if (!("results" in item)) {
+      tags_errors.add(item.tag);
+      return true;
+    }
+
+    $.each(item.results, function(file, tests) {
+      $.each(tests, function(id, result) {
+        tn = result.name;
+        if (!(tn in test_row)) {
+          test_row[tn] = {};
+          for (let i = 1; i <= tags.length; i++)
+            test_row[tn][tags[i - 1][0]] = null;
+        }
+        test_row[tn][item.tag] = {
+          "result": result.result,
+          "comment": result.comment
+        };
+      });
+    });
+  });
+
+  // Remove all rows but first (leave headers)
+  $("#results tr").remove();
+  // Display
+  let table = document.getElementById("results");
+
+  let header = table.insertRow();
+  header.insertCell(0); // name
+  for (let i = 0; i < tags.length; i++) {
+    let cell = header.insertCell(i + 1);
+    cell.innerHTML = "<a href=\"https://github.com/multipath-tcp/mptcp_net-next/actions/runs/" + tags[i][1] + "\" target=\"_blank\">" + tags[i][0] + "</a>";
+    cell.setAttribute("style", "writing-mode: tb-rl; font-size: 1em; padding: 0px;");
+  }
+
+  for (let tn in test_row) {
+    let row = table.insertRow();
+    let name = row.insertCell(0);
+
+    name.innerHTML = tn;
+    name.setAttribute("style", "padding: 0px");
+
+    for (let i = 0; i < tags.length; i++) {
+      let cell = row.insertCell(i + 1);
+      if (tags[i][0] in tags_errors)
+        colorify(cell, {"result": "fail", "comment": "Global error"});
+      else
+        colorify(cell, test_row[tn][tags[i][0]]);
+    }
+  }
+}
+
+function do_it()
+{
+  $(document).ready(function() {
+    let branch = document.getElementById("branch").value;
+    let mode = document.getElementById("mode").value;
+
+    $.get("results/" + branch + "/" + mode + ".json", load_result_table)
+  });
+}
